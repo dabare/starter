c41ad249b204a38733c621263b147f45574cf4ff
Madhava_09466 <madhava.dabare@dialog.lk>
2019-11-30 22:14:12 +0530

    first

---
+++ guiThread.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/mitchellh/go-ps"
+	"log"
+	"os/exec"
+	"time"
+)
+
+var root = `starter\inventory.exe`
+var url = `http://server:7778`
+
+func checkGUI() {
+	pid := startGUI()
+	process, _ := ps.FindProcess(pid)
+	for true {
+		if pid == -1 || process == nil {
+			log.Println("GUI was killed")
+			pid = startGUI()
+		}
+		//log.Println(pid)
+		process, _ = ps.FindProcess(pid)
+		//log.Println(process)
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+//`--kiosk-printing` ,`--disable-print-preview`, `--use-system-default-printer`,
+func startGUI() int {
+	cmd := exec.Command(root, `--new-window`, `--kiosk`, `--suppress-message-center-popups`, `--user-data-dir=`+root+`Data`, url)
+	if err := cmd.Run(); err != nil {
+		log.Println("Error:", err)
+		return -1
+	}
+	log.Println("GUI started")
+	return cmd.Process.Pid
+}
