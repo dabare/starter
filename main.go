edca86c5d9115a7562d69ebdffbde105705c3584
dabare <dabaremadhava@gmail.com>
2019-12-15 08:20:14 +0530

    now runs only one instance

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
 )
 
 func main() {
-	checkGUI()
+	go checkGUI()
+	err := http.ListenAndServe(":9090",nil)
+	if err!= nil {
+		log.Fatal(err)
+	}
 }
